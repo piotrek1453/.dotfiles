8fcb47759589223c78726f859459dad8ba9d5c39
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2026-04-24 18:30:47 +0200

    replace nodejs with npm for void and set it up in install

--- home/.install.sh
+++ home/.install.sh
@@ -22,7 +22,9 @@ void)
 		xargs sudo xbps-install -Sy -u
 	rustup-init -y --default-toolchain nightly --profile default
 	# install js stuff not in Void repos
-	npm install -g markdownlint-cli2 prettier
+	pnpm setup
+	source "$REPO_ROOT/home/.config/fish/config.fish"
+	pnpm install --global markdownlint-cli2 prettier
 	# install vscode
 	pushd home/ || exit
 	./.install_vscode.sh
