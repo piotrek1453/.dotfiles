4aae87e8c51f892286b83e15b3b807b921b3fddb
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2026-04-24 17:22:16 +0200

    making the rice also work for Void: take two

--- home/.install.sh
+++ home/.install.sh
@@ -7,27 +7,26 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 REPO_ROOT="$(git -C "$SCRIPT_DIR" rev-parse --show-toplevel)"
 cd "$REPO_ROOT"
 
-# install system packages
+# install system packages + setup rust/rustup
 source /etc/os-release
 echo "Detected OS: ${NAME}"
 case "$ID" in
 arch)
 	awk '!/^\s*($|#)/' "$REPO_ROOT/home/.arch_packages.txt" |
 		paru -S --needed --noconfirm -
+	rustup toolchain install nightly
+	rustup default nightly
 	;;
 void)
 	awk '!/^\s*($|#)/' "$REPO_ROOT/home/.void_packages.txt" |
-		xargs sudo xbps-install -Sy
+		xargs sudo xbps-install -Sy -u
+	rustup-init -y --default-toolchain nightly --profile default
 	;;
 *)
 	echo "Unsupported OS: ${NAME}"
 	exit 1
 	;;
 esac
-
-# set Rust toolchain to nightly
-rustup toolchain install nightly
-rustup default nightly
 
 # install fisher and tide theme
 curl -sL https://git.io/fisher | fish -c "source; fisher install jorgebucaran/fisher; fisher install IlanCosman/tide@v6"
